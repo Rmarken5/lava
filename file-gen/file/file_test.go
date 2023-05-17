0ea88bfc35408cb9c19ea7b09eec930b9ceab6e8
Ryan Marken <marken.ryan@gmail.com>
2023-05-16 21:27:12 -0400

    Working file

---
+++ file-gen/file/file_test.go
@@ -0,0 +1,93 @@
+package file
+
+import (
+	"github.com/rmarken5/lava/file-gen/strukt"
+	"testing"
+)
+
+func TestPrintFile(t *testing.T) {
+	struktBuilder1 := func(b *strukt.StruktBuilder) *strukt.Strukt { return nil }
+	struktBuilder2 := func(b *strukt.StruktBuilder) *strukt.Strukt { return nil }
+
+	tests := []struct {
+		name           string
+		fileBuilder    func(fb *FileBuilder) *File
+		expectedOutput string
+		expectedError  bool
+	}{
+		{
+			name: "basic file",
+			fileBuilder: func(fb *FileBuilder) *File {
+				return fb.
+					WithPackage("main").
+					WithImports([]string{"fmt"}).
+					WithStruktBuilder(struktBuilder1).
+					WithStruktPrinter(func(b strukt.BuildStrukt) ([]byte, error) {
+						return []byte{}, nil
+					}).
+					Build()
+			},
+			expectedOutput: `package main
+        
+        
+        imports (
+            "fmt"
+        )`,
+			expectedError: false,
+		},
+		{
+			name: "file with struct builders",
+			fileBuilder: func(fb *FileBuilder) *File {
+				return fb.
+					WithPackage("main").
+					WithImports([]string{"fmt"}).
+					WithStruktBuilder(struktBuilder1).
+					WithStruktBuilder(struktBuilder2).
+					WithStruktPrinter(func(b strukt.BuildStrukt) ([]byte, error) {
+						return []byte{}, nil
+					}).
+					Build()
+			},
+			expectedOutput: `package main
+        
+        
+        imports (
+            "fmt"
+        )`,
+			expectedError: false,
+		},
+		{
+			name: "file with invalid template",
+			fileBuilder: func(fb *FileBuilder) *File {
+				return fb.
+					WithPackage("main").
+					WithImports([]string{"fmt"}).
+					WithStruktBuilder(struktBuilder1).
+					WithStruktPrinter(func(b strukt.BuildStrukt) ([]byte, error) {
+						return []byte{}, nil
+					}).
+					Build()
+			},
+			expectedOutput: "",
+			expectedError:  true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			output, err := PrintFile(test.fileBuilder)
+
+			if test.expectedError && err == nil {
+				t.Errorf("expected error but got none")
+			}
+
+			if !test.expectedError && err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+
+			if string(output) != test.expectedOutput {
+				t.Errorf("expected\n%s\nbut got\n%s", test.expectedOutput, string(output))
+			}
+		})
+	}
+}
